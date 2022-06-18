dde39226a53df53d7a23bda23e158325921d6356
Mathias <mathias.b@hotmail.no>
2022-06-18 15:23:28 +0200

    Satt opp skallet til videre utvikling

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import Typical from "react-typical";
+import Switch from "react-switch";
+
+export default function Header({props}) {
+  return (
+    <div>
+        {props}
+    </div>
+  )
+}
