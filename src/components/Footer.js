0b3fc1fa3ef0e92c426405c2b9e4b9ffd5256116
Mathias <mathias.b@hotmail.no>
2022-06-27 19:44:57 +0200

    footer bruker nå tailwind, prettier er nå konfigurert og i bruk

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { Icon } from '@iconify/react';
-
+import React from "react";
+import { Icon } from "@iconify/react";
 
 export default function Footer() {
   return (
-      <div className='footer aligner ' style={{flexDirection: "column"}}>
-
-            <div style={{marginBottom: 15}}>
-              <a href='https://github.com/Datamus' target="_blank">
-                <Icon icon="akar-icons:github-fill" color="white" width="25" height="25" style={{marginRight: 20}}/>
-              </a>
-              <a href='https://www.linkedin.com/in/mathias-simonsen-6723541a3/' target="_blank">
-                <Icon icon="akar-icons:linkedin-box-fill" color="white" width="25" height="25"/>
-              </a>
-            </div>
-
-            <div>
-              <p style={{fontSize: 15, color: "white"}}>Copyright &copy; Mathias Simonsen</p>
-            </div>
-
+    <div className="footer flex items-center justify-center flex-col">
+      <div className="flex" style={{ marginBottom: 15 }}>
+        <a href="https://github.com/Datamus" target="_blank">
+          <Icon
+            icon="akar-icons:github-fill"
+            color="white"
+            width="25"
+            height="25"
+            style={{ marginRight: 20 }}
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mathias-simonsen-6723541a3/"
+          target="_blank"
+        >
+          <Icon
+            icon="akar-icons:linkedin-box-fill"
+            color="white"
+            width="25"
+            height="25"
+          />
+        </a>
       </div>
-  )
+
+      <div>
+        <p style={{ fontSize: 15, color: "white" }}>
+          Copyright &copy; Mathias Simonsen
+        </p>
+      </div>
+    </div>
+  );
 }
