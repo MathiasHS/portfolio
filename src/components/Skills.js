85d1c2cd976f36210e1cf5aefe1e7fbb32cfb400
Mathias <mathias.b@hotmail.no>
2022-06-29 22:23:34 +0200

    Fikset på tittel styling

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import SkillsList from './SkillsList';
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import SkillsList from "./SkillsList";
 
 export default function Skills(props) {
   return (
-    <Container fluid className='aligner skills' style={{flexDirection: "column"}}>
-
-      <Container fluid className='aligner' style={{marginBottom: 50}}>
-        <h1 className="title-styles"style={{color: "white"}}>
-          Skills
+    <Container
+      fluid
+      className="aligner skills"
+      style={{ flexDirection: "column" }}
+    >
+      <Container fluid className="aligner" style={{ marginBottom: 50 }}>
+        <h1 className=" text-lg tracking-widest" style={{ color: "white" }}>
+          SKILLS
         </h1>
       </Container>
 
       <SkillsList />
-      
     </Container>
-  )
+  );
 }
