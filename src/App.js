5a920276bb80fc7ec4153522208ad216f724fbef
Mathias <mathias.b@hotmail.no>
2022-07-01 00:21:42 +0200

    arbeiderfaring bruker props, gg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Skills from "./components/Skills";
@@ -9,15 +9,13 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 
 export default function App() {
-  // const [data, setData] = useState(portfolioData);
-
   return (
     <>
       <Header name={portfolioData.basic_info.name} />
       <About about={portfolioData.basic_info.about} />
       <Projects />
       <Skills skills={portfolioData.skills} />
-      <Experience />
+      <Experience work={portfolioData.experience} />
       <Footer
         name={portfolioData.basic_info.name}
         social={portfolioData.basic_info.social}
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,141 +6,32 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import { Icon } from "@iconify/react";
 
-export default function Experience() {
+export default function Experience(props) {
   return (
     <div className="bg-primaryBeige">
       <div className=" flex py-12  justify-center text-lg tracking-widest">
         <h1>Erfaring</h1>
       </div>
       <VerticalTimeline>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: "rgb(255, 255, 255)",
-            color: "#212529",
-          }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-          date="Juni 2022 - August 2022"
-          iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
-          icon={<Icon icon="mdi:forklift" />}
-        >
-          <h3 className=" font-bold text-lg vertical-timeline-element-title">
-            Lagermedarbeider
-          </h3>
-          <h4 className="">NDI Norge AS</h4>
-          <p>Truckjøring, plukking og pakking av varer. Logistikk.</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: "rgb(255, 255, 255)",
-            color: "#212529",
-          }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-          date="Mars 2021 - November 2021"
-          iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
-          icon={<Icon icon="fluent:paint-brush-16-regular" />}
-        >
-          <h3 className=" font-bold text-lg vertical-timeline-element-title">
-            Sprøytemaler
-          </h3>
-          <h4 className="">Overflatebehandling Sør AS</h4>
-          <p>Behandling av kledning og lister til hus og hytte.</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: "rgb(255, 255, 255)",
-            color: "#212529",
-          }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-          date="Desember 2020 - Januar 2021"
-          iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
-          icon={<Icon icon="codicon:tools" />}
-        >
-          <h3 className=" font-bold text-lg vertical-timeline-element-title">
-            Hjelpearbeider
-          </h3>
-          <h4 className="">Byggmester Per Otto Ingebretsen AS</h4>
-          <p>Etterisolering av bygg.</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: "rgb(255, 255, 255)",
-            color: "#212529",
-          }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-          date="Juni 2020 - Juli 2020"
-          iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
-          icon={<Icon icon="material-symbols:carpenter-outline-sharp" />}
-        >
-          <h3 className=" font-bold text-lg vertical-timeline-element-title">
-            Hjelpearbeider
-          </h3>
-          <h4 className="">VEF Entreprenør AS</h4>
-          <p>Tømrerarbeid, anhuker og rydding.</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: "rgb(255, 255, 255)",
-            color: "#212529",
-          }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-          date="Juni 2018 - Juli 2018"
-          iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
-          icon={<Icon icon="material-symbols:carpenter-outline-sharp" />}
-        >
-          <h3 className=" font-bold text-lg vertical-timeline-element-title">
-            Hjelpearbeider
-          </h3>
-          <h4 className="">VEF Entreprenør AS</h4>
-          <p>Tømrerarbeid, anhuker og rydding.</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: "rgb(255, 255, 255)",
-            color: "#212529",
-          }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-          date="Juni 2016 - Desember 2016"
-          iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
-          icon={<Icon icon="mdi:gantry-crane" />}
-        >
-          <h3 className=" font-bold text-lg vertical-timeline-element-title">
-            Kranfører
-          </h3>
-          <h4 className="">Glencore Nikkelverk AS</h4>
-          <p>Kjørte traverskran. Høsting av nikkelplater.</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{
-            background: "rgb(255, 255, 255)",
-            color: "#212529",
-          }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-          date="Juni 2015 - August 2015"
-          iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
-          icon={<Icon icon="carbon:tool-kit" />}
-        >
-          <h3 className=" font-bold text-lg vertical-timeline-element-title">
-            Vedlikeholdsarbeider
-          </h3>
-          <h4 className="">Kristiansand Dyrepark AS</h4>
-          <p>
-            Ansvar for pH-verdien i vannet og pumpene i badelandet.
-            Vaktmesterarbeid rundt om i parken.
-          </p>
-        </VerticalTimelineElement>
+        {props.work.companies.map((exp) => (
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{
+              background: "rgb(255, 255, 255)",
+              color: "#212529",
+            }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
+            date={exp.date}
+            iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
+            icon={<Icon icon={exp.icon} />}
+          >
+            <h3 className=" font-bold text-lg vertical-timeline-element-title">
+              {exp.position}
+            </h3>
+            <h4 className="">{exp.name}</h4>
+            <p>{exp.description}</p>
+          </VerticalTimelineElement>
+        ))}
 
         <VerticalTimelineElement
           iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
