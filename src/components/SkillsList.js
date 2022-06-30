b22306007481cf16271e76bed306268f091f0219
Mathias <mathias.b@hotmail.no>
2022-06-30 20:06:55 +0200

    SkillsList gjort om til å bruke Tailwind

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,26 +1,25 @@
-import React, {useState} from 'react'
-import { Container, Row } from 'react-bootstrap'
-import SkillItem from './SkillItem.js'
-import {portfolioData} from '../data/portfolioData.js'
-
+import React from "react";
+import SkillItem from "./SkillItem.js";
+import { portfolioData } from "../data/portfolioData.js";
 
 export default function SkillsList(props) {
-
-  
   return (
-        <Row>
-          {portfolioData.map((info, index) =>
-            <ul key={info.skills.icons.toString()} style={{listStyleType: "none", justifyContent: "space-around"}} >
-              {info.skills.icons.map((skills, index) => 
-                <li key={skills.name.toString()} style={{float: "left", marginRight: "1rem", marginLeft: "1rem"}}>
-                  <SkillItem
-                    name={skills.name}
-                    icon={skills.icon}
-                  />
-                </li>
-              )}
-            </ul>
-          )} 
-        </Row>
+    <div className="flex flex-row">
+      {portfolioData.map((info) => (
+        <ul
+          key={info.skills.icons.toString()}
+          style={{ listStyleType: "none", justifyContent: "space-around" }}
+        >
+          {info.skills.icons.map((skills) => (
+            <li
+              key={skills.name.toString()}
+              style={{ float: "left", marginRight: "1rem", marginLeft: "1rem" }}
+            >
+              <SkillItem name={skills.name} icon={skills.icon} />
+            </li>
+          ))}
+        </ul>
+      ))}
+    </div>
   );
 }
