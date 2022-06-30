9ced0de280de8383107935d341bfa0618b6b1b1f
Mathias <mathias.b@hotmail.no>
2022-06-30 20:04:18 +0200

    Skillitem gjort om til å bruke tailwind

--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import { Icon } from '@iconify/react'
- 
+import React from "react";
+import { Icon } from "@iconify/react";
+
 export default function SkillItem(props) {
   return (
     <>
-      <Container className='skills-tile'>
-          <Container className='aligner' >
-              <Icon icon={props.icon} width={40} color={"white"} style={{margin: 5}} />
-          </Container>
-          <Container className='aligner' style={{color: "white", minHeight: 35 }}>
-              <p style={{fontSize: 16}}>{props.name}</p>
-          </Container>
-      </Container>
+      <div className="skills-tile">
+        <div className="flex items-center justify-center">
+          <Icon
+            icon={props.icon}
+            width={40}
+            color={"white"}
+            style={{ margin: 5 }}
+          />
+        </div>
+        <div className="flex items-center justify-center min-h-[35px] text-white">
+          <p className="text-base">{props.name}</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
