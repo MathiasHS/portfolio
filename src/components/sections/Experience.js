df2707cdf1771a7adea995abd14da67df8c6b8fd
Mathias <mathias.b@hotmail.no>
2022-07-13 00:43:54 +0200

    experience component til darkmode

--- src/components/sections/Experience.js
+++ src/components/sections/Experience.js
@@ -8,32 +8,53 @@ import { Icon } from "@iconify/react";
 import SectionTitle from "../utils/SectionTitle";
 
 export default function Experience(props) {
+  const resumeElementBg =
+    localStorage.getItem("theme") === "dark"
+      ? "rgb(223, 223, 223)"
+      : "rgb(255, 255, 255)";
+  const resumeArrowBg =
+    localStorage.getItem("theme") === "dark"
+      ? "7px solid rgb(223, 223, 223)"
+      : "7px solid rgb(255, 255, 255)";
+
+  const iconElementBg =
+    localStorage.getItem("theme") === "dark"
+      ? "rgb(127, 127, 127)"
+      : "rgb(31, 31, 31)";
+
   return (
-    <div className="bg-primaryBeige">
-      <SectionTitle title="Erfaring" />
+    <div className="bg-primaryBeige dark:bg-darkModeColor2">
+      <SectionTitle title="Erfaring" className="dark:text-white" />
       <VerticalTimeline>
         {props.work.companies.map((exp) => (
           <VerticalTimelineElement
+            key={exp.date}
             className="vertical-timeline-element--work"
             contentStyle={{
-              background: "rgb(255, 255, 255)",
+              background: resumeElementBg,
               color: "#212529",
             }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
+            dateClassName="dark:text-white"
+            contentArrowStyle={{
+              borderRight: resumeArrowBg,
+            }}
             date={exp.date}
-            iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
+            iconStyle={{
+              background: iconElementBg,
+              color: "#fff",
+            }}
             icon={<Icon icon={exp.icon} />}
           >
             <h3 className=" font-bold text-lg vertical-timeline-element-title">
               {exp.position}
             </h3>
-            <h4 className="">{exp.name}</h4>
+            <h4>{exp.name}</h4>
             <p>{exp.description}</p>
           </VerticalTimelineElement>
         ))}
 
         <VerticalTimelineElement
-          iconStyle={{ background: "rgb(31, 31, 31)", color: "#fff" }}
+          iconStyle={{ background: iconElementBg, color: "#fff" }}
           icon={<Icon icon="ant-design:hourglass-outlined" />}
         />
       </VerticalTimeline>
