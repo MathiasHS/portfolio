85395901840c65c878840ed76f7c61f1eec8d82b
Mathias <mathias.b@hotmail.no>
2022-07-14 19:36:26 +0200

    polaroid icons er hvite på darkmode

--- src/components/utils/Polaroid.js
+++ src/components/utils/Polaroid.js
@@ -2,6 +2,38 @@ import React from "react";
 import TopBarDots from "./TopBarDots";
 import { Icon } from "@iconify/react";
 export default function Polaroid() {
+  const favTechIcons = () => {
+    if (localStorage.getItem("theme") === "dark") {
+      return (
+        <div className="inline-flex">
+          <Icon
+            icon="akar-icons:react-fill"
+            height="40"
+            color="white"
+            className="m-1"
+          />
+          <Icon icon="bxl:java" height="40" color="white" className="m-1" />
+          <Icon
+            icon="ion:logo-javascript"
+            height="40"
+            color="white"
+            className="m-1"
+          />
+          <Icon icon="cib:mysql" height="40" color="white" className="m-1" />
+        </div>
+      );
+    } else {
+      return (
+        <div className="inline-flex">
+          <Icon icon="logos:react" height="40" className="m-1" />
+          <Icon icon="logos:java" height="40" className="m-1" />
+          <Icon icon="logos:javascript" height="40" className="m-1" />
+          <Icon icon="logos:mysql" height="40" className="m-1" />
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="border-2 dark:border-none shadow ">
       <TopBarDots className="bg-slate-50 dark:bg-darkModeColor4 border-b-2 dark:border-none" />
@@ -9,12 +41,7 @@ export default function Polaroid() {
         <div className="flex mb-4 justify-center items-center ">
           <img width={180} src="images/image_me.png" alt="Avatar placeholder" />
         </div>
-        <div className="inline-flex">
-          <Icon icon="logos:react" height="40" className="m-1" color="white" />
-          <Icon icon="logos:java" height="40" className="m-1" />
-          <Icon icon="logos:javascript" height="40" className="m-1" />
-          <Icon icon="logos:mysql" height="40" className="m-1" />
-        </div>
+        <div className="inline-flex">{favTechIcons()}</div>
       </div>
     </div>
   );
